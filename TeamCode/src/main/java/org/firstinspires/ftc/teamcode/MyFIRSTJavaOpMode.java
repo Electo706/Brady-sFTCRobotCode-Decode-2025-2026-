48da04d02b853314769df6b7289c151c5d76c351
Electo706 <br3husong@gmail.com>
2026-03-18 21:32:21 -0500

    sync files for robot code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Basic Drive", group = "Test")
+public class MyFIRSTJavaOpMode extends LinearOpMode {
+    private DcMotor frontLeft, frontRight;
+    private DcMotor backLeft, backRight;
+
+
+ @Override
+    public void runOpMode() {            //Initialization
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+
+            double lStickY = gamepad1.left_stick_y;
+            double lStickX = gamepad1.left_stick_x;
+            double rStickX = gamepad1.right_stick_x;
+            double rStickY = gamepad1.right_stick_y;
+
+
+            frontLeft.setPower(lStickY + lStickX - rStickX);
+            backLeft.setPower(lStickY - lStickX - rStickX);
+            frontRight.setPower(-lStickY - lStickX - rStickX);
+            backRight.setPower(-lStickY + lStickX - rStickX);
+
+
+        }
+}
+    }
