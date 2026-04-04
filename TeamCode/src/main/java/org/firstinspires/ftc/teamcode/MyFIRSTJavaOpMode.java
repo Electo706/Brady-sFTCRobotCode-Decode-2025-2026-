1c208d4cc2adef17f912f6d5b8f4632dc5490d74
Electo706 <br3husong@gmail.com>
2026-04-04 18:40:28 -0500

    added telemetry and controller button methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -9,44 +9,66 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class MyFIRSTJavaOpMode extends LinearOpMode {
     private DcMotor frontLeft, frontRight;
     private DcMotor backLeft, backRight;
+    private DcMotor intakeMotor;
+    private DcMotor launcherRight, launcherLeft;
 
 
  @Override
-    public void runOpMode() {            //Initialization
+    public void runOpMode() {            //Waits till Driver presses Initialize
+        telemetry.addData("Status", "Initialized");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
+        //intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        //launcherRight = hardwareMap.get(DcMotor.class, "launcherRight");
+        //launcherLeft = hardwareMap.get(DcMotor.class, "launcherLeft");
 
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.FORWARD);
 
-        System.out.println("Front Left Motor: " + frontLeft);
-        System.out.println("Front Right Motor: " + frontRight);
-        System.out.println("Back Left Motor: " + backLeft);
-        System.out.println("Back Right Motor: " + backRight);
+        telemetry.addData("Front Left Motor: ", frontLeft.getPower());
+        telemetry.addData("Front Right Motor: ", frontRight.getPower());
+        telemetry.addData("Back Left Motor: ", backLeft.getPower());
+        telemetry.addData("Back Right Motor: ", backRight.getPower());
+        telemetry.update();
 
 
-        waitForStart();
+        waitForStart(); //Waits until Drivers presses Start
 
         while (opModeIsActive()) {
+            telemetry.addData("Status","OpMode Running");
+            telemetry.addData("Front Left Motor: ", frontLeft.getPower());
+            telemetry.addData("Front Right Motor: ", frontRight.getPower());
+            telemetry.addData("Back Left Motor: ", backLeft.getPower());
+            telemetry.addData("Back Right Motor: ", backRight.getPower());
+            telemetry.update();
 
 
-            double lStickY = gamepad1.left_stick_y;
+            double lStickY = gamepad1.left_stick_y; //Methods for DriveTrain Motors
             double lStickX = gamepad1.left_stick_x;
             double rStickX = gamepad1.right_stick_x;
             double rStickY = gamepad1.right_stick_y;
 
 
-            frontLeft.setPower(lStickY + lStickX - rStickX);
+            frontLeft.setPower(lStickY + lStickX - rStickX); //Calculations for DriveTrain Motors
             backLeft.setPower(lStickY - lStickX - rStickX);
             frontRight.setPower(-lStickY - lStickX - rStickX);
             backRight.setPower(-lStickY + lStickX - rStickX);
 
-            System.out.println("Running Teleop");
+            boolean lBumper = gamepad1.left_bumper; // Bumpers are true or false values
+            boolean rBumper = gamepad1.right_bumper;
 
+
+            double lTrigger = gamepad1.left_trigger; //Triggers are decimal values
+            double rTrigger = gamepad1.right_trigger;
+
+            boolean aButton = gamepad1.a; // X on DualSense
+            boolean bButton = gamepad1.b; // Circle on DualSense
+            boolean xButton = gamepad1.x; // Square on DualSense
+            boolean yButton = gamepad1.y; // Triangle on DualSense
 
         }
 }
